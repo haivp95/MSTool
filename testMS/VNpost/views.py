8ea4a3eac45cc88ca1db0ea78e2ea5377b7786ed
Hung Nguyen Ngoc (VN) <Hung.NguyenN2@hcg.homecredit.net>
2018-06-22 16:58:02 +0700

    update

--- testMS/VNpost/views.py
+++ testMS/VNpost/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
+from django.db import connection
 import requests
 import json
+import cx_Oracle
 
 def APIvnpost(request):
-    response = requests.get('https://ctt.vnpost.vn/serviceApi/v1/getDelivery?token=ca608900-02a0-4a0d-aefa-6ad4ae9a5793&fromRecord=0')
-    vnpAPI = response.json()
+    # response = requests.get('https://ctt.vnpost.vn/serviceApi/v1/getDelivery?token=ca608900-02a0-4a0d-aefa-6ad4ae9a5793&fromRecord=0')
+    # vnpAPI = response.json()
+    # return render(request, 'IndexVNP.html', {
+    #     'Deliverys' : vnpAPI['Deliverys'],
+    # })
+
+    con = cx_Oracle.connect('APP_REPORT_INT[AP_OPS]/Xcvert6uiopp@DBHDWVN-VIETTEL.PROD.ITC.HCNET.VN:1521/HDWVN.HOMECREDIT.VN')
+    print (con.version)
+    cursor = con.cursor()
+    cursor.execute('select * from T_OPS_MIS_CUSTOMER_FEEDBACK')
+    list = dictfetchall(cursor)
+    cursor.close()
+    con.close
     return render(request, 'IndexVNP.html', {
-        'Deliverys' : vnpAPI['Deliverys'],
+        'Deliverys' : list,
     })
+
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
 
